f4476366f2402197341b68b94813a8e5ee8f0221
SimonPreisinger <simonpreisinger@live.de>
2018-01-06 01:21:30 +0100

    simple packet transport

--- src/DatenuebertragungUDP/FileReceiverThread.java
+++ src/DatenuebertragungUDP/FileReceiverThread.java
@@ -46,21 +46,23 @@ public class FileReceiverThread extends Thread {
 
                 // figure out response
                 String dString = null;
+                /*
                 if (in == null)
                     dString = new Date().toString();
                 else
                     dString = getNextQuote();
 
                 buf = dString.getBytes();
-
-		// send the response to the client at "address" and "port"
+*/
+	//	 send the response to the client at "address" and "port"
                 InetAddress address = packet.getAddress();
                 int port = packet.getPort();
                 packet = new DatagramPacket(buf, buf.length, address, port);
                 socket.send(packet);
+                
             } catch (IOException e) {
                 e.printStackTrace();
-		moreQuotes = false;
+		moreQuotes = true;
             }
         }
         socket.close();
--- src/DatenuebertragungUDP/FileSender.java
+++ src/DatenuebertragungUDP/FileSender.java
@@ -13,11 +13,13 @@ import java.nio.file.Path;
 
 
 
+
 // DataOutputSteam verwenden
 // ByteArrayOutputStream.toByteArray() verwenden
 // Prüfsumme berechnen: CRC32 Interface Checksum
 public class FileSender {
-
+	static int smallPacketSize = 2;
+	static byte[] smallPacketsBuffer = new byte[smallPacketSize];
     static BufferedReader in = null;
 	public static void main(String[] args)throws IOException {
 		System.out.println("Hello FileSender benötigt Argumente HostName + Dateiname");
@@ -48,37 +50,67 @@ public class FileSender {
 	            System.err.println(dateiName + " existiert nicht");
 	        }	       	     
 
-	        Path path = Paths.get("JPEG_example.jpg");
-	        byte[] body = Files.readAllBytes(path);
-	        System.out.println("JPEG_example.jpg byte size: " + body.length);
+	        FileReader reader = new FileReader(dateiName);
+
+	        Path filePath = Paths.get(dateiName);
 	        
+	        byte[] fileBuffer = Files.readAllBytes(filePath);
+	        FileInputStream inputStream = new FileInputStream(filePath.toString());
+	        File file = filePath.toFile();       
+	        
+	        DataInputStream dataIn = new DataInputStream(inputStream);
+	        dataIn.readFully(fileBuffer = new byte[(int) file.length()]);
+
+	        //DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("JPEG_example.jpg"));
+	        //dataOut.writeByte(1);
+	        /*
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream(160);
+	        System.out.println("bOutputHeader " +baos.toByteArray());
+	        baos.write(address.getAddress(),0,address.getAddress().length);
+	        baos.toByteArray();
+*/
+	        System.out.println("JPEG_example.jpg byte size: " + fileBuffer.length + " file lenght " + file.length());
+	        int c = 0;
+	        for (int i = 0; i<smallPacketSize; i++)
+	        {
+	        	smallPacketsBuffer[c] = fileBuffer[i];
+	               c++;
+	               if(i%smallPacketsBuffer.length-1==0){
+	       	        
+	                   DatagramPacket packet = new DatagramPacket(smallPacketsBuffer, smallPacketSize, address, 4445);
+	                   smallPacketsBuffer = new byte[smallPacketSize];
+	                   c=0;
+	                   socket.send(packet);
+	                   System.out.println("sent a mini-packet");
+	               }
+	        }
+	        /*
 	        byte[] header = new byte[256];
 	        System.out.println("Inet4address " + address.getAddress().length);
 	        int lenght = address.getAddress().length;
 	        
-	        DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("JPEG_example.jpg"));
-	        //dataOut.writeByte(1);
+
 	        DataInputStream dataIn = new DataInputStream(new FileInputStream("JPEG_example.jpg"));
 
-	        ByteArrayOutputStream bOutputHeader = new ByteArrayOutputStream(160);
-	        System.out.println("bOutputHeader " +bOutputHeader.toByteArray());
-	        bOutputHeader.write(address.getAddress(),0,address.getAddress().length);
-	        bOutputHeader.toByteArray();
+
 
 
 	        
 	       DatagramPacket packet = new DatagramPacket(bOutputHeader.toByteArray(), bOutputHeader.toByteArray().length, address, 4445);
 	       socket.send(packet);
-	    
+	*/    
+	        
+	        
 	           // get response
-	       packet = new DatagramPacket(buf, buf.length);
+	       DatagramPacket packet = new DatagramPacket(buf, buf.length);
 	       socket.receive(packet);
 
 	       // display response
 	       String received = new String(packet.getData(), 0, packet.getLength());
 	       System.out.println("Quote of the Moment: " + received);
 	    
-	       socket.close();
+	       //socket.close();
+	       
 	   
 	
 	//QuoteClientEnd
